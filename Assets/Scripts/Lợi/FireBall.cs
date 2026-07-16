7d29bc960830f23f1fe07ba755efea244a707215
dothanhloi0902 <dothanhloilqmobile@gmail.com>
2026-07-16 18:33:50 +0700

    fix_boss

--- Assets/Scripts/Lợi/FireBall.cs
+++ Assets/Scripts/Lợi/FireBall.cs
@@ -3,28 +3,38 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class FireBall : MonoBehaviour
 {
-    [SerializeField] private int damage = 2; // Sát thương quả cầu lửa
-    [SerializeField] private float lifetime = 4f; // Tự hủy sau 4 giây nếu rơi ra ngoài bản đồ
+    [SerializeField] private int damage = 2;
+    [SerializeField] private float lifetime = 4f;
 
     private void Start()
     {
-        // Tự động hủy để tránh rác bộ nhớ
         Destroy(gameObject, lifetime);
+    }
+
+    // Boss sẽ gọi hàm này sau khi Instantiate
+    public void SetDamage(int newDamage)
+    {
+        damage = newDamage;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Nếu chạm trúng Player
         if (collision.CompareTag("Player"))
         {
-            collision.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
-            Destroy(gameObject); // Chạm trúng thì nổ, biến mất
+            LoiPlayer player = collision.GetComponent<LoiPlayer>();
+
+            if (player != null)
+            {
+                player.TakeDamage(damage);
+
+                Debug.Log("Fire Rain gây " + damage + " damage");
+            }
+
+            Destroy(gameObject);
         }
-        // Nếu chạm trúng đất (Ground)
         else if (((1 << collision.gameObject.layer) & LayerMask.GetMask("Ground")) != 0)
         {
-            // Tại đây bạn có thể tạo thêm hiệu ứng nổ nếu muốn
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
     }
 }
